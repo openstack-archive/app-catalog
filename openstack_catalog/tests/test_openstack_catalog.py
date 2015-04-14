cd153637128cd80ba291577a7874cd36a6a63b32
Herman Narkaytis <hnarkaytis@mirantis.com>
2015-04-15 01:19:11 +0300

    Implement schema validation for murano_apps.json
    Change-Id: I46a8bc16bf4aa72400cefa5a3cc5694bc743e4f0

--- openstack_catalog/tests/test_openstack_catalog.py
+++ openstack_catalog/tests/test_openstack_catalog.py
@@ -19,10 +19,60 @@ test_openstack_catalog
 Tests for `openstack_catalog` module.
 """
 
-from openstack_catalog.tests import base
+import functools
+import json
+
+import jsonschema
+import six
+import testtools
 
 
-class TestOpenstack_catalog(base.TestCase):
+def dict_raise_on_duplicates(ordered_pairs):
+    """Reject duplicate keys."""
+    d = {}
+    for k, v in ordered_pairs:
+        if k in d:
+            raise ValueError("duplicate key: %s (value: %s)" % (k, v))
+        else:
+            d[k] = v
+    return d
 
-    def test_something(self):
-        pass
+
+class TestOpenstack_catalog(testtools.TestCase):
+    def setUp(self):
+        super(TestOpenstack_catalog, self).setUp()
+
+    def _read_raw_file(self, file_name):
+        if six.PY3:
+            opener = functools.partial(open, encoding='utf8')
+        else:
+            opener = open
+        with opener(file_name, 'r') as content_file:
+            return content_file.read()
+
+    def _read_file(self, file_name):
+        return json.loads(self._read_raw_file(file_name))
+
+    def _verify_json_duplicate_keys(self, file_name):
+        try:
+            json.loads(self._read_raw_file(file_name),
+                       object_pairs_hook=dict_raise_on_duplicates)
+        except ValueError as e:
+            self.fail(e)
+
+    def _verify_json_by_schema(self, file_name, schema):
+        data = self._read_file(file_name)
+        schema = self._read_file(schema)
+        try:
+            jsonschema.validate(data, schema)
+        except jsonschema.ValidationError as e:
+            self.fail(e)
+
+    def test_murano_apps_duplicate_keys(self):
+        self._verify_json_duplicate_keys(
+            'openstack_catalog/web/static/murano_apps.json')
+
+    def test_murano_apps_schema_conformance(self):
+        self._verify_json_by_schema(
+            'openstack_catalog/web/static/murano_apps.json',
+            'openstack_catalog/web/static/murano_apps.schema.json')
