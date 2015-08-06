774afae187b0a80e904c53fb1d17701513f9aa5d
Kevin Fox <kevin@efox.cc>
2015-08-06 06:28:25 -0700

    Add a test server
    The start of a test server that allows testing the Heat UI locally.
    Regular SimpleHTTP won't work due to lack of CORS headers.
    
    Change-Id: I95a472e3e0fabad49fd03d1a54956ed0e1f1667e

---
+++ run_tests.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -o errexit
+
+function usage {
+  echo "Usage: $0 [OPTION]..."
+  echo "Run App Catalog's test suite(s)"
+  echo ""
+  echo "  --runserver              Run the development server for"
+  echo "                           openstack_catalog in the virtual"
+  echo "                           environment."
+  echo "  -h, --help               Print this usage message"
+  echo ""
+  exit
+}
+
+# DEFAULTS FOR RUN_TESTS.SH
+#
+root=`pwd -P`
+venv=$root/.venv
+
+runserver=0
+testopts=""
+testargs=""
+
+# Jenkins sets a "JOB_NAME" variable, if it's not set, we'll make it "default"
+[ "$JOB_NAME" ] || JOB_NAME="default"
+
+function process_option {
+  case "$1" in
+    -h|--help) usage;;
+    --runserver) runserver=1;;
+    -*) testopts="$testopts $1";;
+    *) testargs="$testargs $1"
+  esac
+}
+
+# PROCESS ARGUMENTS, OVERRIDE DEFAULTS
+for arg in "$@"; do
+    process_option $arg
+done
+
+function run_server {
+  echo "Starting development server..."
+  pushd $root/openstack_catalog/web > /dev/null
+  for x in murano_apps heat_templates glance_images; do
+    python $root/tools/yaml2json.py < static/$x.yaml > static/$x.json
+  done
+  ${command_wrapper} python $root/tools/testserver.py runserver $testopts $testargs
+  popd > /dev/null
+  echo "Server stopped."
+}
+
+# Development server
+if [ $runserver -eq 1 ]; then
+    run_server
+    exit $?
+fi
+
