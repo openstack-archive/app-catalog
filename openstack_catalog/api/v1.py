63c05b9ba59ef034f330e1da39c6418265c43c78
Kevin Fox <kevin@efox.cc>
2015-09-25 06:33:27 -0700

    WIP - Python Server
    This switches the server over to being Python rather then just
    doing static files. This allows for:
     * Dynamic API.
     * Faster loads due to supporting compression of Javascript.
     * Javascript/CSS from local webserver rather then remote.
     * XStatic managed Javascript/CSS.
     * Sharing code between the app-catalog and Horizon.
    
    This patch set enables most of the infrastructure to do that
    and copies in the initial website. Minimal changes have been
    made to the site to make it compatible with the new code.
    
    Further reviews will switch the site to use more of the new
    functionality.
    
    Change-Id: I2fa9686d3e5db4ee43a02aa7d09b998472e02bf6

---
+++ openstack_catalog/api/v1.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from flask import request
+from flask import Response
+
+from openstack_catalog.api import api
+from openstack_catalog.api import cors_allow
+
+from openstack_catalog import settings
+
+@api.route('/v1')
+def v1_index():
+    data = "assets\nmurano_repo\n"
+    resp = Response(data, status=200, mimetype='plain/text')
+    cors_allow(resp)
+    return resp
+
+
+@api.route('/v1/assets', methods=['GET', 'OPTIONS'])
+def assets_index():
+    if request.method == 'OPTIONS':
+        data = ''
+    else:
+        f = open(settings.ASSETS_FILE, 'r')
+        data = f.read()
+        f.close()
+    resp = Response(data, status=200, mimetype='application/json')
+    resp.headers['Access-Control-Max-Age'] = '3600'
+    resp.headers['Cache-Control'] = 'max-age=3600'
+    cors_allow(resp)
+    return resp
+
+
+@api.route('/v1/murano_repo/<release>/<path:path>')
+def murano_repo_index(release, path):
+    resp = Response('', status=302)
+    resp.headers['Location'] = "http://storage.apps.openstack.org/{}".format(path)
+    return resp
---
+++ openstack_catalog/settings.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""
+Django settings for openstack_catalog project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+import os
+import os.path
+
+from static_settings import get_staticfiles_dirs
+
+ASSETS_FILE = 'openstack_catalog/web/api/v1/assets'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                           'web',
+                                           'static'))
+STATICFILES_DIRS = get_staticfiles_dirs(STATIC_URL)
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+SECRET_KEY = 'notused'
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = []
+
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+TEMPLATE_DIRS = (
+    PROJECT_PATH + '/templates/',
+)
+
+COMPRESS_ENABLED = False
+COMPRESS_CSS_HASHING_METHOD = 'hash'
+COMPRESS_PARSER = 'compressor.parser.HtmlParser'
+
+COMPRESS_OFFLINE_CONTEXT = {
+    'STATIC_URL': STATIC_URL,
+}
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
+    'compressor',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+)
+
+ROOT_URLCONF = 'openstack_catalog.urls'
+WSGI_APPLICATION = 'openstack_catalog.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Override some values from local_settings.py if found
+try:
+    from openstack_catalog.local_settings import *
+except ImportError:
+    pass
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
