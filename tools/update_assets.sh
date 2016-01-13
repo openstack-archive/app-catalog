2e94124c7470e0d4f0ad6e29a07f185e921cec9d
Kevin Fox <kevin@efox.cc>
2016-01-13 10:40:14 -0800

    Derive asset last modified date/time from git logs
    This is a feature to allow the last modified asset time to be
    automatically tracked by the system to allow features such as
    sorting assets by last modified time, or showing the top ten
    newest assets.
    
    Change-Id: Ie6037f575e561d61f48d5e1c21999acfd2d61dc5

--- tools/update_assets.sh
+++ tools/update_assets.sh
@@ -3,11 +3,13 @@ ROOTDIR=`pushd $(dirname $0)/.. > /dev/null /dev/null; pwd; popd > /dev/null`
 echo $ROOTDIR
 pushd $ROOTDIR/openstack_catalog/web > /dev/null
 mkdir -p api/v1/
-if [ ! -f api/v1/assets ] || [ static/assets.yaml -nt api/v1/assets ];
+if [ ! -f api/v1/assets ] || [ static/assets.yaml -nt api/v1/assets ] || [ static/assets_merge.yaml -nt api/v1/assets ];
 then
-	python $ROOTDIR/tools/yaml2json.py < static/assets.yaml > /tmp/assets.$$
+	$ROOTDIR/tools/asset_history.sh static/assets.yaml > static/assets_merge.yaml
+	python $ROOTDIR/tools/yaml2json.py static/assets_merge.yaml < static/assets.yaml > /tmp/assets.$$
 	zopfli --i150 -c /tmp/assets.$$ > /tmp/assets.$$.gz || gzip -c /tmp/assets.$$ > /tmp/assets.$$.gz
 	mv /tmp/assets.$$.gz api/v1/assets.gz
 	mv /tmp/assets.$$ api/v1/assets
+	rm -f static/assets_merge.yaml
 fi
 popd > /dev/null
