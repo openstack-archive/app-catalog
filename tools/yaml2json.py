e8911b45e10bf6eaf72a4f4f2e25a85ada3a17da
Kevin Fox <kevin@efox.cc>
2016-01-13 10:40:14 -0800

    Derive asset last modified date/time from git logs
    This is a feature to allow the last modified asset time to be
    automatically tracked by the system to allow features such as
    sorting assets by last modified time, or showing the top ten
    newest assets.
    
    Change-Id: Ie6037f575e561d61f48d5e1c21999acfd2d61dc5

--- tools/yaml2json.py
+++ tools/yaml2json.py
@@ -13,9 +13,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 import json
 import sys
 import yaml
+
+parser = argparse.ArgumentParser(description='Merge and convert yaml to json.')
+parser.add_argument('files', metavar='F', type=str, nargs='+',
+                    help='file to process')
+
+args = parser.parse_args()
+
+for f in args.files:
+    merge = yaml.load(open(f))['assets']
+
+
+def dict_merge(a, b):
+    for k, v in b.items():
+        if isinstance(v, dict):
+            n = a.setdefault(k, {})
+            merge(v, n)
+        else:
+            a[k] = v
+    return a
 
 y = yaml.load(sys.stdin)
 for a in y['assets']:
@@ -23,6 +43,8 @@ for a in y['assets']:
     if s['type'] == 'heat':
         if 'environment' in s:
             s['environment'] = yaml.dump(s['environment'])
+    if a['name'] in merge:
+        dict_merge(a, merge[a['name']])
 y = [a for a in y['assets'] if a.get('active', True)]
 y = {'assets': y}
 json.dump(y, sys.stdout)
