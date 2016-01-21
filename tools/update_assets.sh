adf319e8d143d50945eb5780aa126b0cc66e7a52
Christopher Aedo <doc@aedo.net>
2016-01-20 18:44:58 -0800

    Include assets_dead.yaml in asset merge
    This will include assets_dead.yaml in the merge/creation of the assets
    JSON file.  The assets_dead.yaml file will be created by proposal_bot
    if/when any dead links are found when the periodic job runs.
    
    Change-Id: I508fef420fe074e215ad996437a55191f647ab5b

--- tools/update_assets.sh
+++ tools/update_assets.sh
@@ -6,7 +6,7 @@ mkdir -p api/v1/
 if [ ! -f api/v1/assets ] || [ static/assets.yaml -nt api/v1/assets ];
 then
 	$ROOTDIR/tools/asset_history.sh static/assets.yaml > static/assets_merge.yaml
-	python $ROOTDIR/tools/yaml2json.py static/assets_merge.yaml < static/assets.yaml > /tmp/assets.$$
+	python $ROOTDIR/tools/yaml2json.py static/assets_merge.yaml static/assets_dead.yaml < static/assets.yaml > /tmp/assets.$$
 	zopfli --i150 -c /tmp/assets.$$ > /tmp/assets.$$.gz || gzip -c /tmp/assets.$$ > /tmp/assets.$$.gz
 	mv /tmp/assets.$$.gz api/v1/assets.gz
 	mv /tmp/assets.$$ api/v1/assets
