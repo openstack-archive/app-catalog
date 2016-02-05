12c70332b50da057ddb5a59849dd122352359bd0
Kirill Zaitsev <k.zaitsev@me.com>
2016-02-05 20:51:44 +0300

    [WIP] glare artifacts plugin for app-catalog asset types
    Change-Id: I8c42f272a58d294475b5a18a8723ba737c0bff41

---
+++ contrib/glare/assets/tosca/v1/template.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2016 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from assets.common import AssetArtifact
+
+from glance.common.artifacts import definitions
+
+
+class TOSCATemplateAsset(AssetArtifact):
+    __endpoint__ = 'tosca_templates'
+
+    template_format = definitions.String(mutable=False, required=True,
+                                         allowed_values=["yaml", "csar"])
+    ever = definitions.Array(
+        item_type=definitions.Dict(
+            properties={
+                'min': definitions.Array(min_size=3, max_size=3,
+                                         item_type=definitions.Integer()),
+                'max': definitions.Array(min_size=3, max_size=3,
+                                         item_type=definitions.Integer()),
+            },
+        )
+    )
