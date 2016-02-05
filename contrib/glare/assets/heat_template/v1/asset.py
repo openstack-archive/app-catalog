c18d36b3cf4c54eb285a5bab2722ef9171884ab0
Kirill Zaitsev <k.zaitsev@me.com>
2016-02-05 20:51:44 +0300

    Add glare artifacts plugin for app-catalog asset types
    Targets-bp: glare-work
    
    Change-Id: I8c42f272a58d294475b5a18a8723ba737c0bff41

---
+++ contrib/glare/assets/heat_template/v1/asset.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2016 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from assets.common import AssetArtifact
+
+from glance.common.glare import definitions
+
+
+class HeatTemplateAsset(AssetArtifact):
+    __endpoint__ = 'heat_templates'
+
+    environment = definitions.Dict()
+    template_version = definitions.String(
+        allowed_values=[
+            '2013-05-23', '2014-10-16', '2015-04-30', '2015-10-15'
+        ],
+        mutable=False)
+
+    template_format = definitions.String(
+        mutable=False, required=True, default="HOT")
