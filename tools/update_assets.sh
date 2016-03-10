23184fe6084126ed6950708f82724e4a80958206
Christopher Aedo <doc@aedo.net>
2016-03-10 15:39:29 -0800

    Move and keep assets_merge.yaml
    This commit changes where assets_merge.yaml file is created, and does
    not delete that file after update_assets.sh is run. The file will now
    be created in the /tmp directory where it will not cause git to think
    any new files have been added in subsequent pull events, and also
    leaves this file in place so someone from the infra team can SSH to
    the box and validate whether or not the fields are being populated
    correctly on the production server.
    
    This will not resolve the linked bug, but will be a big aid in
    tracking down the cause.
    
    Change-Id: I9c9f3f80b5e702e6c259787df741a5f963286688
    Partial-Bug: 1553572

--- tools/update_assets.sh
+++ tools/update_assets.sh
@@ -9,11 +9,10 @@ then
 fi
 if [ ! -f api/v1/assets ] || [ static/assets.yaml -nt api/v1/assets ] || [ static/assets_dead.yaml -nt api/v1/assets ];
 then
-	$ROOTDIR/tools/asset_history.sh static/assets.yaml > static/assets_merge.yaml
-	python $ROOTDIR/tools/yaml2json.py static/assets_merge.yaml static/assets_dead.yaml < static/assets.yaml > /tmp/assets.$$
+	$ROOTDIR/tools/asset_history.sh static/assets.yaml > /tmp/assets_merge.yaml
+	python $ROOTDIR/tools/yaml2json.py /tmp/assets_merge.yaml static/assets_dead.yaml < static/assets.yaml > /tmp/assets.$$
 	zopfli --i150 -c /tmp/assets.$$ > /tmp/assets.$$.gz || gzip -c /tmp/assets.$$ > /tmp/assets.$$.gz
 	mv /tmp/assets.$$.gz api/v1/assets.gz
 	mv /tmp/assets.$$ api/v1/assets
-	rm -f static/assets_merge.yaml
 fi
 popd > /dev/null
