546b1fa678bfd2901e6288f53365930cb7d0a5a9
Omar Shykhkerimov <oshykhkerimov@mirantis.com>
2016-03-16 20:42:30 +0200

    Create form for making assets
    Work in progress
    
    Change-Id: I2f44b20b4cd06e415596a881ee5c7be5ed120a27

--- openstack_catalog/urls.py
+++ openstack_catalog/urls.py
@@ -3,6 +3,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from openstack_catalog import views
 
-urlpatterns = [url(r'^$', views.index)]
+urlpatterns = [url(r'^$', views.index),
+               url(r'^create_asset$', views.create_asset)]
 
 urlpatterns += staticfiles_urlpatterns()
