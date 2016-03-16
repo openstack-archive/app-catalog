fcd07af8c01efbbfa0c96758a86a5dbade1db083
Omar Shykhkerimov <oshykhkerimov@mirantis.com>
2016-03-16 20:42:30 +0200

    Create form for making assets
    Work in progress
    
    Change-Id: I2f44b20b4cd06e415596a881ee5c7be5ed120a27

--- openstack_catalog/views.py
+++ openstack_catalog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+def create_asset(request):
+    return render(request, 'create_asset.html', {})
