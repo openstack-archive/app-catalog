f64cfcbfd54946661ef01f43993a410cf7d928e3
Konstantin Snihyr <ksnihyr@mirantis.com>
2016-04-26 16:50:59 +0300

    Add asset edit feature
    Change-Id: I4121903dd59de2ef93a8a9e6b73aea7ad0b47105

--- openstack_catalog/web/static/apps/manage-glare-assets/js/glareassets.addasset.controller.js
+++ openstack_catalog/web/static/apps/manage-glare-assets/js/glareassets.addasset.controller.js
@@ -1,5 +1,5 @@
 (function () {
-  "use strict";
+  //"use strict";
 
   angular
     .module('glareassets')
@@ -9,6 +9,9 @@
     '$scope',
     '$rootScope',
     '$http',
+    '$interpolate',
+    '$location',
+    '$cookies',
     'AssetTypesFactory'
   ];
 
@@ -17,6 +20,8 @@
     $scope.asset = {};
     $scope.asset.version = "0.0.0";
     $scope.typeSpecific = {};
+    $scope.loadedAsset = {};
+    $scope.operation = "Add asset";
     $scope.releases = {
       'Austin': false,
       'Bexar': false,
@@ -36,7 +41,51 @@
     };
   }
 
-  function addAssetController($scope, $rootScope, $http, AssetTypesFactory) {
+  function addAssetController($scope, $rootScope, $http, $interpolate, $location, $cookies, AssetTypesFactory) {
+    $scope.$on('$locationChangeStart', function () {
+        initEmptyAsset($scope);
+        var asset_id = $cookies.asset_id;
+        if (!!asset_id) {
+          var asset_version = $cookies.asset_version;
+          var asset_name = $cookies.asset_name;
+          $scope.operation = "Edit asset";
+          var t_name = {
+            'MuranoPackageAsset': 'murano_packages',
+            'GlanceImageAsset': 'glance_images',
+            'BundleAsset': 'bundles',
+            'TOSCATemplateAsset': 'tosca_templates',
+            'MyArtifact': 'myartifact'
+          }[asset_name];
+          var t_version = 'v' + asset_version;
+          var assetLinkTemplate = $interpolate('/api/v2/db/artifacts/{{type}}/{{id}}/{{asset_id}}');
+          $scope.assetUrl = assetLinkTemplate({
+            type: t_name,
+            id: t_version,
+            asset_id: asset_id
+          });
+          $http.get($scope.assetUrl)
+          .success(function (data) {
+            $scope.asset = data;
+            $scope.loadedAsset = JSON.parse(JSON.stringify(data));
+            for (var i=0; i < $scope.asset.release.length; ++i) {
+              $scope.releases[$scope.asset.release[i]] = true;
+            }
+            delete $scope.asset['id'];
+            delete $scope.asset['type_name'];
+            delete $scope.asset['deleted_at'];
+            delete $scope.asset['created_at'];
+            delete $scope.asset['published_at'];
+            delete $scope.asset['depends'];
+            delete $scope.asset['type_version'];
+            delete $scope.asset['updated_at'];
+            $cookies.asset_id = '';
+
+          });
+        } else {
+          $scope.operation = "Add asset";
+        }
+      }
+    );
 
     $scope.assetTypes = {
       types: [],
@@ -49,8 +98,15 @@
         $scope.assetTypes.selected = $scope.assetTypes.types[0];
       }
     });
+    
+    $scope.$watch('asset', function () {
+      $scope.assetTypes.selected = $scope.assetTypes.types.filter(function (item) {
+        return item.name == $scope.asset.type_name + ": v" + $scope.asset.type_version;
+      })[0];
+    });
 
-    initEmptyAsset($scope);
+
+
 
     $scope.assetSubmit = function() {
       var data = angular.copy($scope.asset);
@@ -82,46 +138,45 @@
 
       console.log(dataToSend);
       console.log($scope.fileData);
-
       $http({
-        method: "POST",
-        url: glareUrl,
-        data: data,
-        headers: {
-          "Content-Type": "application/json"
-        }
-      }).success(function(result) {
-        $rootScope.$broadcast('assetAdded');
-        console.log(result);
-        var glareBlobUrl = '/api/v2/db/artifacts/' + type.type + '/' + type.id + '/' + result.id + '/stored_object';
-        console.log(glareBlobUrl);
+          method: "POST",
+          url: glareUrl,
+          data: data,
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        .success(function(result) {
+          $rootScope.$broadcast('assetAdded');
+          console.log(result);
+          var glareBlobUrl = '/api/v2/db/artifacts/' + type.type + '/' + type.id + '/' + result.id + '/stored_object';
+          console.log(glareBlobUrl);
+          var blobMessage = '';
 
-        var blobMessage = '';
-
-        // also upload the binary if it's there
-        if (data.stored) {
-          $http({
-            method: "PUT",
-            url: glareBlobUrl,
-            data: $scope.fileData.assetFile.data,
-            headers: {
-              "Content-Type": "application/octet-stream"
-            }
-          }).success(function(blobResult) {
-            console.log(blobResult);
-            blobMessage = 'Binary has been uploaded successfully.';
-          }).error(function(blobResult) {
-            console.log(blobResult);
-            blobMessage = 'There was an error uploading the binary.';
-          });
+          // also upload the binary if it's there
+          if (data.stored) {
+            $http({
+              method: "PUT",
+              url: glareBlobUrl,
+              data: $scope.fileData.assetFile.data,
+              headers: {
+                "Content-Type": "application/octet-stream"
+              }
+            }).success(function(blobResult) {
+              console.log(blobResult);
+              blobMessage = 'Binary has been uploaded successfully.';
+            }).error(function(blobResult) {
+              console.log(blobResult);
+              blobMessage = 'There was an error uploading the binary.';
+            });
         }
-        alert('Artefact draft successfully created. ' + blobMessage);
-        initEmptyAsset($scope);
-      }).error(function(result) {
-        console.log(result);
-        alert('Artefact creation failed.');
-      });
-    };
+          alert('Artefact draft successfully created. ' + blobMessage);
+          initEmptyAsset($scope);
+        }).error(function(result) {
+          console.log(result);
+          alert('Artefact creation failed.');
+        });
+    }
   }
 })();
 
