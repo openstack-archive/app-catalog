604a1f2a9f0ab41c6cc32ef0d9855caec3230f14
Sergey Skripnick <sskripnick@mirantis.com>
2016-07-20 15:58:22 +0300

    [wip] Glare v1.0 artifact types and service proxy
    Also added data migration script
    
    Co-Authored-By: Kirill Zaitsev <kzaitsev@mirantis.com>
    
    Change-Id: Ib5c6920c90f566d73a5f9e8ed545f0e082a0b20e

---
+++ contrib/glare/openstack_app_catalog/artifacts.py
@@ -0,0 +1,200 @@
+# Copyright (c) 2016 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import shutil
+import tempfile
+
+from glance.db.glare import artifact_api
+from glance.objects import base
+from glance.objects import fields as glare_fields
+from glance.objects.fields import attribute
+from toscaparser.tosca_template import ToscaTemplate
+
+from oslo_versionedobjects import fields
+
+
+def dict_validator(allowed_keys=None, required_keys=None, field_name=None):
+    def validator(field):
+        if allowed_keys:
+            unexpected_keys = set(field.keys()) - allowed_keys
+            if unexpected_keys:
+                fmt = "Unexpected keys: %s (allowed keys %s) for field %s"
+                raise ValueError(fmt % (unexpected_keys, allowed_keys,
+                                        field_name))
+        if required_keys:
+            missing_keys = required_keys - set(field.keys())
+            if missing_keys:
+                raise ValueError("Missing keys: %s" % missing_keys)
+    return validator
+
+
+def values_validator(allowed_values, field_name):
+    def validator(field):
+        if field not in allowed_values:
+            fmt = "Invalid value %s for %s. Allowed values: %s"
+            raise ValueError(fmt % (field, field_name, allowed_values))
+    return validator
+
+_icon_keys = {"url", "top", "left", "height"}
+icon_validator = dict_validator(allowed_keys=_icon_keys,
+                                required_keys=_icon_keys,
+                                field_name="icon")
+_provided_by_keys = {"name", "href", "company"}
+provided_by_validator = dict_validator(allowed_keys=_provided_by_keys,
+                                       required_keys=_provided_by_keys,
+                                       field_name="provided_by")
+supported_by_validator = dict_validator(allowed_keys={"name", },
+                                        required_keys={"name", },
+                                        field_name="supported_by")
+
+tosca_fmt_validator = values_validator(allowed_values={"zip", "yaml", "csar"},
+                                       field_name="template_format")
+heat_tpl_version_validator = values_validator(
+    allowed_values={'2013-05-23', '2014-10-16', '2015-04-30', '2015-10-15'},
+    field_name="template_version")
+
+
+class BaseAsset(base.BaseArtifact):
+    VERSION = '0.1'
+
+    fields = {
+        'depends': attribute(glare_fields.List,
+                             element_type=glare_fields.Dependency,
+                             validators=[],
+                             required_on_activate=False),
+        'provided_by': attribute(glare_fields.Dict,
+                                 validators=[provided_by_validator],
+                                 required_on_activate=True,
+                                 element_type=fields.String),
+        'supported_by': attribute(glare_fields.Dict,
+                                  validators=[supported_by_validator],
+                                  required_on_activate=False,
+                                  element_type=fields.String),
+        'release': attribute(glare_fields.List,
+                             default=[],
+                             validators=[],
+                             required_on_activate=False,
+                             element_type=fields.String),
+        'icon': attribute(glare_fields.Dict,
+                          validators=[icon_validator],
+                          required_on_activate=False,
+                          element_type=fields.String),
+        'license': attribute(fields.StringField,
+                             validators=[],
+                             required_on_activate=True),
+        'license_url': attribute(fields.StringField,
+                                 validators=[],
+                                 required_on_activate=False),
+        'metadata': attribute(glare_fields.Dict,
+                              validators=[],
+                              required_on_activate=False,
+                              element_type=fields.String),
+    }
+
+    @classmethod
+    def get_type_name(cls):
+        return "base_artifact"
+
+    @classmethod
+    def init_db_api(cls):
+        return artifact_api.ArtifactAPI(cls)
+
+
+class GlanceImageAsset(BaseAsset):
+
+    fields = {
+        'container_format': attribute(fields.StringField,
+                                      filter_ops=[],
+                                      required_on_activate=True),
+        'disk_format': attribute(fields.StringField,
+                                 filter_ops=[],
+                                 required_on_activate=True),
+        'min_ram': attribute(fields.IntegerField,
+                             filter_ops=[],
+                             required_on_activate=False),
+        'min_disk': attribute(fields.IntegerField,
+                              filter_ops=[],
+                              required_on_activate=False),
+        'image': attribute(glare_fields.BlobField,
+                           required_on_activate=False,
+                           mutable=False,
+                           sortable=False,
+                           filter_ops=[]),
+        'image_indirect_url': attribute(fields.StringField,
+                                        filter_ops=[],
+                                        required_on_activate=False),
+        }
+
+    @classmethod
+    def get_type_name(cls):
+        return "glance_image"
+
+
+class TOSCATemplateAsset(BaseAsset):
+
+    fields = {
+        'template_format': attribute(fields.StringField,
+                                     validators=[tosca_fmt_validator],
+                                     required_on_activate=True),
+        'template': attribute(glare_fields.BlobField,
+                              required_on_activate=True)
+    }
+
+    @classmethod
+    def get_type_name(cls):
+        return "tosca_template"
+
+    @classmethod
+    def validate_upload(cls, context, af, field_name, fd):
+        suffix = "." + af.template_format
+        tfd = tempfile.NamedTemporaryFile(prefix="aoo_tosca", suffix=suffix)
+        shutil.copyfileobj(fd, tfd)
+        ToscaTemplate(tfd.name)
+        tfd.flush()
+        tfd.seek(0)
+        return tfd
+
+
+class HeatTemplateAsset(BaseAsset):
+
+    fields = {
+        'environment': attribute(glare_fields.Dict,
+                                 required_on_activate=False,
+                                 default={},
+                                 element_type=fields.String),
+        'template': attribute(glare_fields.BlobField,
+                              required_on_activate=True),
+        'files': attribute(glare_fields.Dict,
+                           required_on_activate=False,
+                           default={},
+                           element_type=glare_fields.BlobFieldType),
+    }
+
+    @classmethod
+    def get_type_name(cls):
+        return "heat_template"
+
+
+class MuranoPackageAsset(BaseAsset):
+
+    fields = {
+        'package': attribute(glare_fields.BlobField,
+                             required_on_activate=True),
+        'package_name': attribute(fields.StringField,
+                                  required_on_activate=True),
+    }
+
+    @classmethod
+    def get_type_name(cls):
+        return "murano_package"
---
+++ contrib/move_to_glare_10.py
@@ -0,0 +1,210 @@
+# Copyright (c) 2016 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import argparse
+import json
+import logging
+import requests
+import urlparse
+import yaml
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+HEADERS_AUTH = {
+    "x-identity-status": "Confirmed",
+    "x-project-id": "app-catalog",
+    "content-type": "application/json",
+}
+HEADERS_PATCH = HEADERS_AUTH.copy()
+HEADERS_BLOB = HEADERS_AUTH.copy()
+HEADERS_PATCH["content-type"] = "application/json-patch+json"
+HEADERS_BLOB["content-type"] = "application/octet-stream"
+
+
+def _get_keys(src, dst, *keys):
+    for key in keys:
+        if key in src:
+            dst[key] = src[key]
+
+
+class AssetUploader(object):
+
+    def __init__(self, glare_url, filename, download):
+        self._filename = filename
+        self._glare_url = glare_url
+        self._download = download
+        self._assets = {}
+        self._uploaded = {}
+
+    def _get_url(self, *args):
+        return urlparse.urljoin(self._glare_url, "/".join(args))
+
+    def parse(self):
+        for asset in yaml.safe_load(open(self._filename))["assets"]:
+            if asset["name"] in self._assets:
+                raise Exception("Duplicate name %s" % asset["name"])
+            self._assets[asset["name"]] = asset
+
+    def upload_assets(self):
+        for name in self._assets:
+            try:
+                self.upload_asset(name)
+            except Exception:
+                logging.exception("Failed to create asset %s" % name)
+                return
+        for name, asset in self._assets.items():
+            self._set_dependencies(asset)
+        for asset_type, asset_id in self._uploaded.values():
+            r = self._patch(asset_type, asset_id, [{
+                "op": "replace",
+                "path": "/status",
+                "value": "active",
+            }])
+            if r.status_code != 200:
+                logging.error("Failed to activate %s" % asset_id)
+
+    def upload_asset(self, name):
+        if name in self._uploaded:
+            logging.debug("Already uploaded %s" % name)
+            return
+        asset = self._assets[name]
+        artifact_type = asset["service"]["type"]
+        method = getattr(self, "_process_%s" % artifact_type, None)
+        if method:
+            method(asset, self._get_common_data(asset))
+
+    def _create_asset(self, artifact_type, data):
+        logging.info("Creating %s '%s'" % (artifact_type, data["name"]))
+        url = self._get_url("artifacts", artifact_type)
+        r = requests.post(url, data=json.dumps(data), headers=HEADERS_AUTH)
+        if r.status_code != 201:
+            fmt ="Failed to create %s %s"
+            raise Exception(fmt % (artifact_type, r.text))
+        artifact = r.json()
+        self._uploaded[data["name"]] = (artifact_type, artifact["id"])
+        return artifact
+
+    def _patch(self, artifact_type, asset_id, data):
+        url = self._get_url("artifacts", artifact_type, asset_id)
+        r = requests.patch(url, data=json.dumps(data),
+                           headers=HEADERS_PATCH)
+        return r
+
+    def _activate_asset(self, artifact_type, asset_id):
+        url = self._get_url("artifacts", artifact_type, asset_id)
+        data = json.dumps([{
+            "op": "replace",
+            "path": "/status",
+            "value": "active",
+        }])
+        loggng.debug('Activating asset %s' % asset_id)
+        r = requests.patch(url, data=data, headers=HEADERS_PATCH)
+        if r.status_code != 200:
+            logging.error(r.text)
+            raise Exception("Failed to activate artifact %s" % asset_id)
+        return r
+
+    def _create_blob(self, asset_type, asset_id, blob_name, blob_url):
+        asset_url = self._get_url("artifacts", asset_type, asset_id, blob_name)
+        if self._download:
+            data = requests.get(blob_url)
+            r = requests.put(asset_url, data=data, headers=HEADERS_BLOB)
+        else:
+            data = {
+                "external": True,
+                "url": blob_url,
+                "status": "active",
+            }
+            r = requests.put(asset_url, data=json.dumps(data),
+                             headers=HEADERS_AUTH)
+        logging.debug("Blob created %s" % r.status_code)
+        return r
+
+    def _get_common_data(self, asset):
+        data = {}
+        _get_keys(asset, data, "name", "description", "provided_by",
+                      "release", "icon", "license", "license_url",
+                      "supported_by")
+        data["metadata"] = asset["attributes"]
+
+
+        return data
+
+    def _set_dependencies(self, asset):
+        if asset["name"] not in self._uploaded:
+            # TODO: bundle
+            return
+        depends = asset.get("depends")
+        if depends:
+            deps = []
+            for item in depends:
+                asset_type, asset_id = self._uploaded[item["name"]]
+                deps.append("/artifacts/%s/%s" % self._uploaded[item["name"]])
+            patch = [{
+                "op": "replace",
+                "path": "/depends",
+                "value": deps,
+            }]
+            logging.debug("Setting deps for %s" % asset["name"])
+            asset_id = self._uploaded[asset["name"]][1]
+            logging.debug(patch)
+            self._patch(asset_type, asset_id, patch)
+
+    def _process_glance(self, asset, data):
+        asset["attributes"].pop("size", None)
+        _get_keys(asset["service"], data,
+                      "min_disk", "min_ram", "disk_format",
+                      "container_format", "image_indirect_url")
+        image_url = asset["attributes"].pop("url", None)
+        image = self._create_asset("glance_image", data)
+        if image_url is None:
+            return
+        image_url = "http://127.0.0.1:8000"
+        self._create_blob("glance_image", image["id"], "image", image_url)
+
+    def _process_tosca(self, asset, data):
+        url = asset["attributes"].pop("url")
+        data["template_format"] = asset["service"]["template_format"]
+        template = self._create_asset("tosca_template", data)
+        self._create_blob("tosca_template", template["id"], "template", url)
+
+    def _process_heat(self, asset, data):
+        url = asset["attributes"].pop("url")
+        _get_keys(asset, data, "environment", "template_version")
+        template = self._create_asset("heat_template", data)
+        self._create_blob("heat_template", template["id"], "template", url)
+
+    def _process_murano(self, asset, data):
+        url = asset["attributes"].pop("Package URL")
+        data["package_name"] = asset["service"]["package_name"]
+        package = self._create_asset("murano_package", data)
+        self._create_blob("murano_package", package["id"], "package", url)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='move assets into glare.')
+    parser.add_argument('--glare_url', metavar='g', type=str,
+                        help='glare_url', default='http://127.0.0.1:9494/')
+    parser.add_argument('--assets_file', metavar='f', type=str,
+                        help='assets_file',
+                        default='openstack_catalog/web/static/assets.yaml')
+    parser.add_argument('--download', action='store_true',
+                        dest='download', help='download')
+    parser.set_defaults(download=False)
+    args = parser.parse_args()
+    uploader = AssetUploader(args.glare_url, args.assets_file,
+                             args.download)
+    uploader.parse()
+    uploader.upload_assets()
