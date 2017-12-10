8b260eaf0895ee8c30a1f8e5fbe9988ee53a9390
Christopher Aedo <doc@aedo.net>
2017-12-10 10:12:12 -0800

    Retiring App Catalog
    This commit removes content associated with the app catalog.
    This is part of the first step, following the guide at:
    https://docs.openstack.org/infra/manual/drivers.html#retiring-a-project
    
    Earlier this year the TC agreed to retire this project:
    https://review.openstack.org/#/c/452086/
    
    In July, Fungi sent
    a note to the operators list as well:
    http://lists.openstack.org/pipermail/openstack-operators/2017-July/013965.html
    
    Depends-On: I52ce13057643d69a0fd87bce20ee460c6b7c2f17
    Change-Id: Idd69df495663e5ab6466c8d06945c9257a93842a

--- openstack_catalog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-from openstack_catalog import views
-
-urlpatterns = [url(r'^$', views.index)]
-
-urlpatterns += staticfiles_urlpatterns()
--- openstack_catalog/wsgi.py
+++
@@ -1,7 +0,0 @@
-from openstack_catalog import wsgi_django
-
-from openstack_catalog.api import api
-
-from werkzeug.wsgi import DispatcherMiddleware
-
-application = DispatcherMiddleware(wsgi_django.application, {'/api': api})
